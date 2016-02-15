da1f815314c283632917a383b4fefd14027241a8
nodes777 <nodes777@gmail.com>
2016-02-15 13:55:45 -0500

    overlays contact text on map, needs rezizing issues to be addressed

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(40.798117, -77.856580), // State College
