3ff13863216d23e6134b0111f0939fd975d9b562
nodes777 <nodes777@gmail.com>
2016-02-27 14:39:13 -0500

    makes subMenu appear

--- js/menuBuilder.js
+++ js/menuBuilder.js
@@ -9,12 +9,31 @@ var getJSON = function(menu) {
        var textNode = document.createTextNode(name);
        elem.appendChild(textNode);
        /*Put that element where it belongs*/
-       document.getElementById("menu").appendChild(elem);
+       document.getElementById("menuMain").appendChild(elem);
        /*Add classes for style*/
-       elem.className = "col-md-3 ";
        elem.className += "menuCategories";
        /*Add  click event to the element*/
-
+       elem.addEventListener("click", function(){
+        /* on click, first clear whatever is there, then display new menu*/
+        document.getElementById("subMenus").innerHTML= "";
+        displaySubMenu(categories);
+        });
     });
 };
+
+var displaySubMenu = function(category){
+    var choices = category.info[0].parts[0].choices;
+    console.log(choices);
+    choices.forEach(function(obj){
+        /*Create an element and give it the name*/
+        var elem = document.createElement("p");
+        var textNode = document.createTextNode(obj.name+" "+obj.price);
+        elem.appendChild(textNode);
+        /*Put it in the new subMenu*/
+        document.getElementById("subMenus").appendChild(elem);
+        /*Add classes for style*/
+       elem.className += "menuCategories";
+    });
+
+}
 getJSON(menu);
