f258612c9ad4659ca0e269d954309aa5fdca237e
nodes777 <nodes777@gmail.com>
2016-03-05 16:23:46 -0500

    adds headings to submenus

--- js/menuBuilder.js
+++ js/menuBuilder.js
@@ -24,6 +24,26 @@ var getJSON = function(menu) {
 
 var displaySubMenu = function(category){
     var partsLength = category.info[0].parts.length;
+    console.log(partsLength);
+    if (partsLength===3){
+                    document.getElementById("subMenu0").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu1").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu2").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu0").className = "col-md-3";
+                    document.getElementById("subMenu1").className = "col-md-3";
+                    document.getElementById("subMenu2").className = "col-md-3";
+                }
+                else if(partsLength===2) {
+                    document.getElementById("subMenu0").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu1").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu0").className = "col-md-4";
+                    document.getElementById("subMenu1").className = "col-md-4";
+                }
+                else{
+                    document.getElementById("subMenu0").innerHTML= "<h2>Choose One</h2>";
+                    document.getElementById("subMenu0").className = "col-md-4";
+                    document.getElementById("subMenu1").className = "col-md-4";
+                }
     for (var i = 0; i < partsLength; i++) {
         var choices = category.info[0].parts[i].choices;
         choices.forEach(function(obj){
@@ -35,14 +55,7 @@ var displaySubMenu = function(category){
             document.getElementById("subMenu" + i).appendChild(elem);
             /*Add classes for style*/
             elem.className += "menuCategories";
-                if (partsLength<3){
-                    document.getElementById("subMenu0").className = "col-md-4";
-                    document.getElementById("subMenu1").className = "col-md-4";
-                }
-                else {
-                    document.getElementById("subMenu0").className = "col-md-3";
-                    document.getElementById("subMenu1").className = "col-md-3";
-                }
+
         });
     }
 }
